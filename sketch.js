d30524ee47eab89a8f1f7f2de228a9a52d2732aa
André Kogos <00001098268179sp@al.educacao.sp.gov.br>
2024-08-05 17:02:48 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function draw() {
+circle(horizontal,vertical,35)
+fill (cor)
+if (mouseX<horizontal){horizontal--}
+  if(mouseX<horizontal){horizontal++}
+  if(mouseY)>vertical){vertical++}
+  if(mouseY)>vertical){vertical++}
+  cor=color(random(0,255),random(0,255),random(0,255),random(0,100));
